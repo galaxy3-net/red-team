a1f4a17be9f8013660a178c1299e992cac4ae092
korbenkirscht <korben@kirscht.com>
2021-12-02 23:06:55 -0800

    resource_group.ps1

--- azure/arm/deny_all_inbound_nsg.ps1
+++ azure/arm/deny_all_inbound_nsg.ps1
@@ -1,3 +1,20 @@
-Get-AzNetworkSecurityGroup -Name "RedTeamSG" -ResourceGroupName "RedTeam"
+#$nsg = Get-AzNetworkSecurityGroup -Name "RedTeamSG" -ResourceGroupName "RedTeam"
 
-Set-AzNetworkSecurityRuleConfig -Name "rdp-rule" -NetworkSecurityGroup $nsg -Access "Deny" -DestinationAddressPrefix * -DestinationPortRange 3389 -Direction Inbound -Name 'rdp-rule' -Priority 4096 -Protocol Tcp  -SourcePortRange *
+#$rule1 = New-AzNetworkSecurityRuleConfig -Name rdp-rule -Description "Allow RDP" `
+#    -Access Allow -Protocol Tcp -Direction Inbound -Priority 100 -SourceAddressPrefix `
+#    Internet -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 3389
+
+#$rule2 = New-AzNetworkSecurityRuleConfig -Name web-rule -Description "Allow HTTP" `
+#    -Access Allow -Protocol Tcp -Direction Inbound -Priority 101 -SourceAddressPrefix `
+#    Internet -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 80
+
+
+
+# https://docs.microsoft.com/en-us/powershell/module/az.network/new-aznetworksecurityruleconfig?view=azps-6.6.0
+
+Get-AzNetworkSecurityGroup -Name ReadTeamSG -ResourceGroupName ReadTeam |
+        Add-AzNetworkSecurityRuleConfig -Name rdp-rule -Description "Allow RDP" -Access `
+    Allow -Protocol Tcp -Direction Inbound -Priority 100 -SourceAddressPrefix Internet `
+    -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 3389 |
+        Set-AzNetworkSecurityGroup
+ #>
