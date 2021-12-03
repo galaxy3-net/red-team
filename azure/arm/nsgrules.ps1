4791a10de85b22606f27910d25e25b281462b613
korbenkirscht <korben@kirscht.com>
2021-12-02 23:53:56 -0800

    resource_group.ps1

--- azure/arm/nsgrules.ps1
+++ azure/arm/nsgrules.ps1
@@ -19,5 +19,5 @@ Get-AzNetworkSecurityGroup -Name RedTeamSG -ResourceGroupName RedTeam |
         Set-AzNetworkSecurityGroup
 #>
 
-Get-AzNetworkSecurityGroup -Name RedTeamSG -ResourceGroupName RedTeam | Add-AzNetworkSecurityRuleConfig -Name allow-rdp -Description "Allow RDP" -Access `  Deny -Protocol * -Direction Inbound -Priority 4097 -SourceAddressPrefix Internet `    -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange * |        Set-AzNetworkSecurityGroup
+Get-AzNetworkSecurityGroup -Name RedTeamSG -ResourceGroupName RedTeam | Add-AzNetworkSecurityRuleConfig -Name allow-rdp -Description "Allow RDP" -Access `  Deny -Protocol * -Direction Inbound -Priority 4094 -SourceAddressPrefix Internet `    -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange * |        Set-AzNetworkSecurityGroup
  #>
