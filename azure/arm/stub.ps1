1632cc35ffa6953bf7a7f5692c500c65029611e6
korbenkirscht <korben@kirscht.com>
2021-12-03 20:12:45 -0800

    resource_group.ps1

--- azure/arm/stub.ps1
+++ azure/arm/stub.ps1
@@ -4,3 +4,5 @@ $ScriptName
 $PUBLIC_IP
 
 env | sort
+
+New-AzResourceGroupDeployment -ResourceGroupName "${ResourceGroupName}" -TemplateUri https://raw.githubusercontent.com/galaxy3-net/red-team/krk/azure/arm/templates/${IEX_MODULE}.json -TemplateParameterUri https://raw.githubusercontent.com/galaxy3-net/red-team/krk/azure/arm/templates/${IEX_MODULE}_parameters.json
